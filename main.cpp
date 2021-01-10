94d802919c1ff0ad2b13ddfc97433040ae0677a5
halilbaydar <halilbaydar@yahoo.com>
2021-01-10 15:43:08 +0300

    bitti.

--- main.cpp
+++ main.cpp
@@ -9,8 +9,9 @@
 #include <map>
 #include <bits/stdc++.h>
 using namespace std;
-int returnMinRow(vector<vector<float>> arr, int whichClass);
-pair<int, int> returnMinMaxofColum(vector<vector<float>> arr, int whichcolumn);
+int returnMinRow(vector<vector<double>> arr, int whichClass);
+pair<int, int> returnMinMaxofColum(vector<vector<double>> arr, int whichcolumn);
+int rankforme = -1;
 int main(int argc, char *argv[])
 {
 
@@ -31,12 +32,16 @@ int main(int argc, char *argv[])
     stringstream ss(line);
     ss >> N >> A >> M >> T;
 
-    float global_dataset[N * A + N];
-    int parts_size = pow((N / (P - 1)), 2) + (N / (P - 1));
-    float local_dataset[parts_size];
+    int parts_size = (N / (P - 1)) * A + (N / (P - 1));
+    double global_dataset[N * A + N + parts_size];
+    double local_dataset[parts_size];
+    for (int i = 0; i < parts_size; i++)
+    {
+        global_dataset[i] = 0;
+    }
     if (rank == 0) // i am in master process
     {
-        int index;
+        int index = parts_size;
         while (getline(input, line))
         {
             stringstream ss(line);
@@ -48,46 +53,51 @@ int main(int argc, char *argv[])
                 ss >> var;
                 try
                 {
-                    global_dataset[index] = stoi(var);
+                    global_dataset[index] = stod(var);
                 }
                 catch (exception e)
                 {
+                    //cout << "arr size küçük";
                     break;
                 }
                 index++;
             } while (ss);
         }
-    }
-    MPI_Scatter(
-        global_dataset, parts_size, MPI_FLOAT,
-        local_dataset, parts_size, MPI_FLOAT,
-        1, MPI_COMM_WORLD);
 
+        ////cout<<"okuma başarılı"<<endl;
+    }
+
+    MPI_Scatter(
+        global_dataset, parts_size, MPI_DOUBLE,
+        local_dataset, parts_size, MPI_DOUBLE,
+        0, MPI_COMM_WORLD);
+    //cout<<"distribute başarılı"<<endl;
     if (rank != 0)
     {
-
-        MPI_Gather(
-            local_dataset, parts_size, MPI_FLOAT,
-            global_dataset, parts_size, MPI_FLOAT,
-            1, MPI_COMM_WORLD);
-
+        rankforme = rank;
+        //cout<<rank<<" işlem yapıyor"<<endl;
         vector<int> selectedindexes;
         int nearestmissrow = 0, nearesthitrow = 0;
-        float manhattandistance[A];
+        double manhattandistance[A];
         for (int iteration = 0; iteration < M; iteration++)
         {
-            vector<vector<float>> twodimensionalarr;
-            vector<vector<float>> twodimensionalarr2;
+            //cout<<"iteration yapılıyor rank: "<<rank<<" iteration : "<<iteration<<endl;
+            vector<vector<double>> twodimensionalarr;
+            vector<vector<double>> twodimensionalarr2;
             int yy = 0;
             for (int i = 0; i < (N / (P - 1)); i++) //converting one dimensional array into two dimensional array
             {
+                vector<double> tempppp;
                 for (int column = 0; column <= A; column++)
                 {
-                    twodimensionalarr[i][column] = local_dataset[yy];
-                    twodimensionalarr2[i][column] = local_dataset[yy];
+                    tempppp.push_back(local_dataset[yy]);
+                    tempppp.push_back(local_dataset[yy]);
                     yy++;
                 }
+                twodimensionalarr2.push_back(tempppp);
+                twodimensionalarr.push_back(tempppp);
             }
+            //cout<<"random row seçiliyor"<<endl;
             int randomrow = rand() % (N / (P - 1)) + 1; //1 den verilen row sayısına kadar
             vector<int>::iterator myit = find(selectedindexes.begin(), selectedindexes.end(), randomrow);
             while (myit != selectedindexes.end())
@@ -96,6 +106,7 @@ int main(int argc, char *argv[])
                 myit = find(selectedindexes.begin(), selectedindexes.end(), randomrow);
             }
             selectedindexes.push_back(randomrow);
+            //cout<<"rank :"<<rank <<"rowlar random rowdan çıkarılıyor"<<endl;
             for (int i = 0; i < (N / (P - 1)); i++)
             {
                 for (int column = 0; column < A && i != randomrow; column++)
@@ -103,72 +114,71 @@ int main(int argc, char *argv[])
                     twodimensionalarr[i][column] -= twodimensionalarr[randomrow][column];
                 }
             }
-
+            //cout<<"rank :"<<rank<<" narest hit ve miss hesaplanıyor"<<endl;
             nearesthitrow = returnMinRow(twodimensionalarr, twodimensionalarr[randomrow][twodimensionalarr[randomrow].size() - 1]);
             nearestmissrow = returnMinRow(twodimensionalarr, 1 - (twodimensionalarr[randomrow][twodimensionalarr[randomrow].size() - 1]));
 
+            //cout<<"nearestmissrow :"<<nearestmissrow << " nearesthitrow :" <<nearesthitrow<<endl;
             for (int stepfour = 0; stepfour < A; stepfour++)
             {
                 /*pair<minindex,maxindex>*/ pair<int, int> minmaxrow = returnMinMaxofColum(twodimensionalarr2, stepfour);
-                float temp = abs(twodimensionalarr2[minmaxrow.first][stepfour] - twodimensionalarr[minmaxrow.second][stepfour]) / M;
-                float temp2 = abs(twodimensionalarr2[randomrow][stepfour] - twodimensionalarr[nearestmissrow][stepfour]);
-                float temp3 = abs(twodimensionalarr2[randomrow][stepfour] - twodimensionalarr[nearesthitrow][stepfour]);
-                manhattandistance[stepfour] -= (temp3 / temp) + (temp2 / temp);
+                //cout<<"minindex : "<<minmaxrow.first<<" maxindex :" <<minmaxrow.second<<endl;
+                double temp = abs(twodimensionalarr2[minmaxrow.first][stepfour] - twodimensionalarr[minmaxrow.second][stepfour]) / M;
+                double temp2 = abs(twodimensionalarr2[randomrow][stepfour] - twodimensionalarr[nearestmissrow][stepfour]);
+                double temp3 = abs(twodimensionalarr2[randomrow][stepfour] - twodimensionalarr[nearesthitrow][stepfour]);
+                manhattandistance[stepfour] = manhattandistance[stepfour]-(temp3 / temp) + (temp2 / temp);
             }
+            //cout<<"rank :"<<rank<<" manhantan distance hesaplandı "<<endl;
         }
-        queue<float> manhattandistance2;
+        for(auto i:manhattandistance)
+            cout<<i<<" ";
+        cout<<endl;
+        set<double> manhattandistance2;
         for (int p = 0; p < A; p++)
         {
-            manhattandistance2.push(manhattandistance[p]);
+            manhattandistance2.insert(manhattandistance[p]);
         }
-        MPI_Send(
-            &manhattandistance2,
-            1,
-            MPI_FLOAT,
-            0,
-            0,
-            MPI_COMM_WORLD);
-        for (int k = 0; k < T; k++)
+        cout << "Slave P" << rank << " :";
+        int oo = 0;
+        for (set<double>::iterator it = manhattandistance2.begin(); it != manhattandistance2.end(); it++)
         {
-            cout << manhattandistance2.front();
-            manhattandistance2.pop();
-        }
-    }
-    if (rank == 0)
-    {
-        set<float> results;
-        for (int i = 1; i <= P; i++)
-        {
-            queue<float> receive;
-            MPI_Recv(
-                &receive,
-                1,
-                MPI_FLOAT,
-                1,
-                0,
-                MPI_COMM_WORLD,
-                MPI_STATUS_IGNORE);
-            for (int t = 0; t < T; t++)
+            for (int j = 0; j < A; j++)
             {
-                results.insert(receive.front());
-                receive.pop();
+                if (*it == manhattandistance[j])
+                {
+                    cout << j << " ";
+                    break;
+                }
             }
+            oo++;
+            if (oo == T)
+                break;
         }
-        for (set<float>::iterator t = results.begin(); t != results.end(); t++)
-            cout << *t < endl;
+        cout << endl;
     }
+    /*
+   if(rank==0){
+       cout<<"rank"<<rank<<endl;
+        for(int i=0; i<parts_size; i++)
+            cout<<local_dataset[i]<<" ";
+        cout<<endl;
+        
+       cout<<parts_size;
+    }
+    */
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
     return 0;
 }
-int returnMinRow(vector<vector<float>> arr, int whichClass)
+int returnMinRow(vector<vector<double>> arr, int whichClass)
 {
     int minrow = INT16_MAX;
     int index = -1;
-    for (int row = 0; row < arr.size(); row++)
+    int row = 0;
+    for (row = 0; row < arr.size(); row++)
     {
         int temp = 0;
-        for (int column = 0; column < arr[row].size() - 1 && arr[row][arr[row].size() - 1] == whichClass; column++)
+        for (int column = 0; column < (arr[row].size() - 1) && arr[row][arr[row].size() - 1] == whichClass; column++)
         {
             temp += arr[row][column];
         }
@@ -181,7 +191,7 @@ int returnMinRow(vector<vector<float>> arr, int whichClass)
     return index;
 }
 
-pair<int, int> returnMinMaxofColum(vector<vector<float>> arr, int whichcolumn)
+pair<int, int> returnMinMaxofColum(vector<vector<double>> arr, int whichcolumn)
 {
     double Min = INT_MAX, Max = INT_MIN;
     int minindex, maxindex;
